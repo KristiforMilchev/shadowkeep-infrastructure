4e88f06ae94fef76d4ebfe6e1ffa15e0767abc15
Kristifor Milchev <km@airzen.io>
2023-06-17 22:11:40 +0300

    Renaming Page to BookPage because of naming issues with the routing package

--- lib/interfaces/ichapter_service.dart
+++ lib/interfaces/ichapter_service.dart
@@ -1,12 +1,12 @@
+import 'package:domain/models/book_page.dart';
 import 'package:domain/models/chapter.dart';
-import 'package:domain/models/page.dart';
 
 abstract class IChapterService {
   Future<bool> setBookId(int bookId);
   Future<bool> addChapter(Chapter chapter);
   Future<bool> editChapter(Chapter chapter);
   Future<bool> deleteChapter(Chapter chapter);
-  Future<List<Page>> getPages(int chapterId);
+  Future<List<BookPage>> getPages(int chapterId);
   Future<List<Chapter>> getChaptersIncludingCharacter(List<int> characters);
 
   Future<List<Chapter>> getAllChapters(int bookId);
--- lib/interfaces/ipage_service.dart
+++ lib/interfaces/ipage_service.dart
@@ -1,10 +1,10 @@
-import 'package:domain/models/page.dart';
+import 'package:domain/models/book_page.dart';
 
 abstract class IPageService {
   Future<bool> setBookId(int bookId);
-  Future<bool> addPage(Page current);
-  Future<Page> getPage(int pageId);
+  Future<bool> addPage(BookPage current);
+  Future<BookPage> getPage(int pageId);
   Future<bool> deletePage(int pageId);
-  Future<bool> editPage(Page page);
-  Future<List<Page>> getChapterPages(int chapterId);
+  Future<bool> editPage(BookPage page);
+  Future<List<BookPage>> getChapterPages(int chapterId);
 }
