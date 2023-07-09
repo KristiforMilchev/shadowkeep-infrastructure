4f2d4d41957d06b871da32251acd5a74010037a1
Kristifor Milchev <km@airzen.io>
2023-07-09 12:05:21 +0300

    Changing the implementation of the observer

--- lib/interfaces/iobserver.dart
+++ lib/interfaces/iobserver.dart
@@ -1,5 +1,5 @@
 abstract class IObserver {
-  subscribe<T>(String callbackLocation, Function fn, {T? data});
-  Function getObserver(String callbackName);
+  void subscribe<T>(String callbackLocation, Function fn, {T? data});
+  void getObserver(String callbackName, dynamic data);
   dispose(String callbackLocation);
 }
